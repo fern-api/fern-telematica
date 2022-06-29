c701329b2d00ab11803b60858dfc43f33d07e72a
Deep Singhvi <dsinghvi@umich.edu>
2022-06-28 19:45:15 -0700

    bootstrap yarn sample app

---
+++ sample-js-app/src/main.ts
@@ -0,0 +1,9 @@
+import { model, services } from "@fern-fern/telematica-api-client";
+import { defaultFetcher } from "@fern-typescript/service-utils";
+
+// const REMOTE_GENERATION_SERVICE = new services.remoteGen.RemoteGenerationService({
+//   origin: "https://fiddle-coordinator-dev.buildwithfern.com/api",
+//   fetcher: defaultFetcher,
+// });
+
+defaultFetcher;
